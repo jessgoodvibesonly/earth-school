90633b8c5045069079b219419486a90d1c5a26c0
Jessgoodvibesonly <jessgoodvibesonly@gmail.com>
2026-04-26 17:08:10 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,7 @@
 export const metadata = {
-  title: "EARTH SCHOOL",
-  description: "A Temporary Human Experience"
+  title: "Earth School",
+  description: "A temporary human experience",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body style={{ margin: 0 }}>{children}</body>
-    </html>
-  );
-}
